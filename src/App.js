e14006964643edfc7400a3963e5acf4392e42ef4
JC Kao <aruby5223@gmail.com>
2022-08-17 11:05:46 +0800

    feat: 建立頁面路由

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import {
   HashRouter,
   NavLink,
+  Routes,
+  Route
 } from 'react-router-dom';
 
 const Todo = () => {
-  return <p>這是 Todo 頁面 
+  return <p>這是 Todo 頁面
   </p>;
 };
 const Login = () => {
@@ -35,6 +37,11 @@ function App() {
         </div>
         {/* Routes, Route 練習區 */}
         {/* 練習區 */}
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/todo" element={<Todo />} />
+        </Routes>
       </HashRouter>
     </div>
   );
